ad4d8c7f4ed31205088a32b77d081db20f1da606
인수심 <simssijjang@naver.com>
2025-11-12 13:41:35 +0900

    Add HuntingGround system with level-based zones

--- Assets/Scripts/World/HuntingGround.cs
+++ Assets/Scripts/World/HuntingGround.cs
@@ -1,10 +1,83 @@
-// HuntingGround.cs
 using UnityEngine;
+using System.Collections.Generic;
 
-public class HuntingGround : MonoBehaviour {
-    public int levelRequirement;
+namespace EducationalRPG.World
+{
+    public class HuntingGround : MonoBehaviour
+    {
+        [Header("Hunting Ground Info")]
+        [SerializeField] private string groundName = "초보자 사냥터";
+        [SerializeField] private int recommendedLevel = 1;
+        [SerializeField] private int maxLevel = 10;
+        
+        [Header("Monster Spawners")]
+        [SerializeField] private List<Enemy.MonsterSpawner> spawners = new List<Enemy.MonsterSpawner>();
+        [SerializeField] private bool autoFindSpawners = true;
+        
+        [Header("Danger Zone")]
+        [SerializeField] private float dangerLevel = 1f;
+        [SerializeField] private Color groundColor = Color.red;
+        
+        private bool playerInGround = false;
 
-    public bool IsPlayerAbleToEnter(Player player) {
-        return player.level >= levelRequirement;
+        private void Start()
+        {
+            if (autoFindSpawners)
+            {
+                spawners.AddRange(GetComponentsInChildren<Enemy.MonsterSpawner>());
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                playerInGround = true;
+                
+                var expSystem = other.GetComponent<Player.ExperienceSystem>();
+                int playerLevel = expSystem != null ? expSystem.CurrentLevel : 1;
+                
+                string message = $"{groundName} (추천 레벨: {recommendedLevel}~{maxLevel})";
+                
+                if (playerLevel < recommendedLevel)
+                {
+                    message += "\n⚠️ 레벨이 낮습니다! 주의하세요!";
+                }
+                else if (playerLevel > maxLevel)
+                {
+                    message += "\n경험치 획득량이 감소합니다.";
+                }
+                
+                Debug.Log(message);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                playerInGround = false;
+            }
+        }
+
+        public bool IsAppropriateLevel(int playerLevel)
+        {
+            return playerLevel >= recommendedLevel && playerLevel <= maxLevel;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = new Color(groundColor.r, groundColor.g, groundColor.b, 0.2f);
+            
+            BoxCollider boxCollider = GetComponent<BoxCollider>();
+            if (boxCollider != null)
+            {
+                Gizmos.matrix = transform.localToWorldMatrix;
+                Gizmos.DrawCube(boxCollider.center, boxCollider.size);
+                
+                Gizmos.color = groundColor;
+                Gizmos.DrawWireCube(boxCollider.center, boxCollider.size);
+            }
+        }
     }
 }
